a489cdcfcf56f3b3af232903e6a1de3d82c6df94
Nelly BORJON-PIRON <nelly.borjon@hotmail.fr>
2020-04-23 11:25:50 +0200

    env done cloudinary almost done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const Sneaker = require('../models/Sneaker.js');
+const Tag = require('../models/Tag.js');
+const User = require('../models/User.js');
+const uploadCloud = require('../config/cloudinary.js');
+
+
+
+
 
 router.get("/", (req, res) => {
   res.send("foo");
