137678faf012214eaa8442a0d627802019ac273d
Teddy ROGERS <teddy.rogers.pro@outlook.com>
2020-04-23 17:04:39 +0200

    signin/signup complete

--- routes/auth.js
+++ routes/auth.js
@@ -65,4 +65,58 @@ router.get("/signin", (req, res) => {
     res.render("signin");
 })
 
+router.post('/signin', (req, res, next) => {
+
+    const theEmail = req.body.email;
+    const thePassword = req.body.password;
+
+    if (theEmail === '' || thePassword === '') {
+        res.render('signin', {
+            errorMessage: 'Fill all the fields...'
+        });
+        return
+    }
+
+    User.findOne({
+            'email': theEmail
+        })
+
+        .then((dbResult) => {
+
+
+            if (!dbResult) {
+                res.render('signin', {
+                    errorMessage: 'Bad account...'
+                });
+                return
+            }
+
+
+
+            if (bcrypt.compareSync(thePassword, dbResult.password)) {
+                req.session.currentUser = dbResult
+                res.redirect('/')
+            } else {
+                res.render('signin', {
+                    errorMessage: 'Bad account...'
+                })
+            }
+        })
+
+        .catch((error) => {
+            next(error)
+        })
+
+})
+
+
+
+router.get('/logout', (req,res) => {
+    req.session.destroy((err) => {
+        res.redirect('/')
+    })
+})
+
+
+
 module.exports = router;
